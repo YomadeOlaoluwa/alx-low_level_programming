06d5cdab072f1ac53f45582051247e1e5e88aa5f
Yomade01 <olaoluwayomade@gmail.com>
2022-09-12 07:38:35 -0700

    task 4 0x00 C-Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Return: by default returns zero if no errors otherwise return non zero value
  */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
