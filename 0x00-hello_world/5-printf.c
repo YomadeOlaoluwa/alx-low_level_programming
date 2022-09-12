1855bde750e60d0752689e14ac112ecb7e938a7d
Yomade01 <olaoluwayomade@gmail.com>
2022-09-12 07:46:43 -0700

    task 5 0x00 C-Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point for the program
+ * return: zero if errors, non zero if errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
